cc8760d3d8536c1ad74cf6a3aaede6db417ae141
Sanyam Saxena <sanyamsxn1998@gmail.com>
2025-11-13 00:01:14 +0530

    signin page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { SignupComponent } from "./pages/signup/signup.component";
+import { SigninComponent } from "./pages/signin/signin.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, SignupComponent],
+  imports: [RouterOutlet, HomeComponent, SignupComponent, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, HeaderComponent],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+  signinForm : FormGroup;
 
+   constructor(private fb: FormBuilder) {
+    // ✅ Initialize it using FormBuilder
+    this.signinForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+ get email() {
+    return this.signinForm.get('email')!;
+  }
+
+  get password() {
+    return this.signinForm.get('password')!;
+  }
+
+  onSubmit(){
+    if(this.signinForm.valid){
+      console.log(this.signinForm.value)
+    }
+  }
 }
